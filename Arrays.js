2a91c6657d90b9a0d14ee6ac29b9a43d2211caa9
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-07 18:42:19 +0530

    Arrays completed

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+let arr=[1,2,3,4,5,5,6,6];
+console.log(arr[2]);
+console.log(arr[arr.length-1]);
+arr.push(100);
+console.log(arr[arr.length-1]);
+
+
+
+
---
+++ StringMethod.js
@@ -0,0 +1,26 @@
+let s="MyAshish      ";
+console.log(s);
+// console.log(s.trim());
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+// //0 based indexing
+// console.log(s.indexOf("z"));
+// console.log(s.indexOf("hish"));
+
+//METHOD CHAINING
+console.log(s.trim().toLocaleUpperCase());
+
+//Slice : Returns a part of original string as new string, LAst index not included.
+// console.log(s.slice(-))
+// console.log(s.slice(2,4));
+// console.log(s.slice(4));
+
+//REPLACE
+//Need to input correct thing else it will not work as intended.small a in the following is different from capital A.
+console.log(s.replace("Ashish","Darshita"));
+console.log(s.replace("A","D"));
+// console.log(s.repeat(1000));
+
+
+///----------------------------------------------
+
