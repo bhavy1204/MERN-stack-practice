14a213e1842828811c284d8a9a0f9e7ee190158c
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-07 19:12:45 +0530

    Object Literals completed

--- Arrays.js
+++ Arrays.js
@@ -1,9 +1,37 @@
 let arr=[1,2,3,4,5,5,6,6];
-console.log(arr[2]);
-console.log(arr[arr.length-1]);
-arr.push(100);
-console.log(arr[arr.length-1]);
+// console.log(arr[2]);
+// console.log(arr[arr.length-1]);
+// arr.push(100);
+// console.log(arr[arr.length-1]);
+
+//MULTI/TWO DIMENSIONAL ARRAYS
+
+let arr2=[[1,2,3],[4,5,6],[7,8,9]];
+
+// console.log(arr2[0]);
+// console.log(arr2[1]);
+// console.log(arr2[2]);
+// console.log(arr2[1][1]);
+// console.log(arr2[2][3]);
+
+// for(let i=0;i<arr2.length;i++){
+//     for(let j=0;j<3;j++){
+//         console.log(arr2[i][j]);
+//     }
+// }
+// for(let i=0;i<=12;i++){
+//     console.log(i+10);
+// }
+
+// let s="123";
+// let n = parseInt(s);
+// console.log(n+10);
+// console.log(s+10);
 
 
-
+// let i=100;
+// while(i!=0){
+//     console.log(i);
+//     i-=10;
+// }
 
---
+++ ObjectLiterals.js
@@ -0,0 +1,9 @@
+//Used to Store key value pair and complex entities
+//Way of storing objects
+// REMBER : OBJECTS ARE DIFFERENT FROM OBJECT LITERALS
+
+// propert=> (key,value);
+//Objects are collection of properties
+
+//Objects have no orders and we can also store arrays in it.
+
