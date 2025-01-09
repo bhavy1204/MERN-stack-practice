5324fb1deaa7d27c42bbe4c199a45811ab53f890
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-09 17:48:15 +0530

    Arrow functions added

---
+++ ArrayMethod.js
@@ -0,0 +1,12 @@
+//They all used concept of callBack
+let arr=[1,2,3,4,5,6,7,8,9,10];
+//1. For Each
+
+
+//Spread
+function display(...arr){
+    console.log(arr+10);
+}
+
+display(arr);
+
---
+++ ArrowFunctions.js
@@ -0,0 +1,32 @@
+//Nameless functions
+//Single argument=>Parathesis optional
+// c=10;
+// const a=(c)=>{console.log(c)};
+// const b=()=>{c}
+// console.log(b);
+// const a=(s)=>{
+//     console.log("Hello, "+s);
+// }
+
+// a("Ashish");
+
+//-----TIME OUT AND SET INTERVAL-----
+
+
+// setTimeout(() => {
+//     console.log("hello");
+// }, 2000);
+
+
+//Function ke baad ka code bhi run hoga but timeout function ek fixed time ke baad hi execute hoga jo apan decide karenge
+
+//Ye mostly API clls aur request response me kaam aata hai.
+
+// let a= setInterval(() => {
+//     console.log("Hii");
+// }, 100000);
+
+// let b=setInterval(()=>{
+//     console.log("Exmple 2");
+// },2000);
+
