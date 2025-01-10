43fba48b7966547dcd5bb3540f46034abd0cc4e3
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-10 23:15:02 +0530

    DOM basics done

---
+++ DOM/dom.js
@@ -0,0 +1,14 @@
+// let a=prompt("ENter Name ");
+// let b=window.document.getElementById("meow");
+// b.innerText=a;
+
+//-------------------------------------------------------------------
+// let sm = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < sm.length; i++) {
+//   setTimeout(() => {
+//     sm[i].src = "spiderman_img.png";
+//   }, 5000);
+// }
+//-------------------------------------------------------------------
+
+
