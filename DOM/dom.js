3063ca757ab4f74397e5e7559c9c569062e9dde1
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-11 11:06:58 +0530

    Added DOM manipulation

--- DOM/dom.js
+++ DOM/dom.js
@@ -11,4 +11,25 @@
 // }
 //-------------------------------------------------------------------
 
+//WE selct elemsts by querySelector and querySelectorAll to select elemts like CSS. We can select by tagname, class or id in it. querySelector selects the first occurence of that elemt in the page like first "p" paragrapgh whereas querySelectorAll selects all the occureence of that elemt like all p tags in the HTML document, QuerySelectorAll givess us a node list and then from that we have to select the elemts by index like arrys:- 
+
+// document.queryselctor('p').innerText="Meow";
+// document.queryselctorAll('p')[0].innerText="Meow";
+
+//--------------------------------------------------------------------------------
+
+//In similar manner we have selectElementById, selctElementByTagName, selectElementByClassName and we use the id, tag and class name in them to select the desired elemnt but as discused earlie we will use query selector the most. So no biggie.
+
+//-----------------------------------------------------------------------------
+//DOM manipulation
+//COMMON PROPERTIES WE WILL USE FRQUENTLY------
+//TEXT AND CONTENT BASED PROP.
+//1. InnerText : text on screen,it understand everything as text cant use yags
+
+//2. InnerHTML : html text/markupp 'a','b'...etc. can use tags in maipulation . Can use back ticks to just maipulate the tags only and j=keeping the text content as it is.
+
+//3. InnerCONTENT : full content with all hidden like '\n','hidden contens/display:none'..etc or (jesa markup me text tha without tags).
+
+
+//-----------------------------------------------------------------------------------
 
