4739d20c0322364ccb8ae84a86f87d799901b294
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-12 18:11:55 +0530

    Basic Events done

--- DOM/Events.js
+++ DOM/Events.js
@@ -1,4 +1,4 @@
-// let p = document.querySelector("p");
+let p = document.querySelector("p");
 
 // p.onclick = function () {
 //   console.log("Color Changed");
@@ -7,15 +7,34 @@
 
 // // p.onclick= changeColor;
 
-// let para = document.querySelectorAll("#description");
+let para = document.querySelectorAll("#description");
 
 // // para.onclick = changeColor;
 // for(pi of para){
 //     pi.onclick = changeColor;
 // }
 
-// function changeColor() {
-//   console.log("USing 'this'");
-//   this.style.color = "purple";
-// }
+function changeColor() {
+  console.log("USing 'this'");
+  this.style.color = "purple";
+}
 //---------------------------------------------------------------------------------
+
+//element.addEventListner(event,callback);
+
+p.addEventListener("mouseenter",changeColor);
+
+let img = document.querySelector('img');
+
+img.addEventListener("mouseenter",changeSize);
+
+function changeSize(){
+    this.style.border="10px solid black";
+}
+ img.addEventListener("click",function(){
+    this.style.borderRadius="20px";
+ })
+img.addEventListener("dblclick",function(){
+    this.style.boxShadow="20px 20px 20px black";
+})
+
