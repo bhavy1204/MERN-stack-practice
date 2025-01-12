ed61016dc524804efc2838cf12acf8f6e242be5e
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-12 17:58:20 +0530

    Stared Events

---
+++ DOM/Events.js
@@ -0,0 +1,21 @@
+// let p = document.querySelector("p");
+
+// p.onclick = function () {
+//   console.log("Color Changed");
+//   p.style.color = "red";
+// }
+
+// // p.onclick= changeColor;
+
+// let para = document.querySelectorAll("#description");
+
+// // para.onclick = changeColor;
+// for(pi of para){
+//     pi.onclick = changeColor;
+// }
+
+// function changeColor() {
+//   console.log("USing 'this'");
+//   this.style.color = "purple";
+// }
+//---------------------------------------------------------------------------------
