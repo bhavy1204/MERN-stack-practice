3ca39af7b420ff0b54288cfc6ce8b3f76da0a65f
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-14 11:20:06 +0530

    HTML and Css revised

--- Advance concepts/promises.js
+++ Advance concepts/promises.js
@@ -2,33 +2,33 @@ let body = document.querySelector("body");
 
 let el = document.createElement("button");
 
-function saveData(data, success, failure) {
-  let speed = Math.floor(Math.random() * 10) + 1;
-  if (speed > 5) {
-    success();
-  } else {
-    failure();
-  }
-}
+// function saveData(data, success, failure) {
+//   let speed = Math.floor(Math.random() * 10) + 1;
+//   if (speed > 5) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
 
 //This is real world example of call back hell.
 
-saveData(
-  "Ok",
-  () => {
-    el.innerText = "Successfully saved";
-    body.prepend(el);
-    saveData("Meow",()=>{
-        console.log("Ok2");
-    },()=>{
-        console.log("Failure 2");
-    })
-  },
-  () => {
-    el.innerText = "Failed";
-    body.prepend(el);
-  }
-);
+// saveData(
+//   "Ok",
+//   () => {
+//     el.innerText = "Successfully saved";
+//     body.prepend(el);
+//     saveData("Meow",()=>{
+//         console.log("Ok2");
+//     },()=>{
+//         console.log("Failure 2");
+//     })
+//   },
+//   () => {
+//     el.innerText = "Failed";
+//     body.prepend(el);
+//   }
+// );
 
 
 //To save us from such nasty call back hells we have promises.
