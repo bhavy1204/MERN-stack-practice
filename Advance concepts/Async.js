9b45e2fa75297524901d7ea30b1da8442f2fd007
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-18 20:16:45 +0530

    Added async and await

--- Advance concepts/Async.js
+++ Advance concepts/Async.js
@@ -1,12 +1,56 @@
 // Async functions are those function s which returns a promise by deafult
 
 async function greet(){
+    // It makes our promise change to rehected state ;
     // throw "error";
     return "Hello ";
 }
 
 greet().then((res)=>{
-    console.log("USer 1 ");
-}).catch(()=>{
-    console.log("Error");
+    console.log(res ,"USer 1");
+    return greet();
+}).then((res)=>{
+    console.log(res ,"USer 2");
 })
+.catch(()=>{
+    console.log("Error");
+});
+
+let temp = async ()=>{
+    return 5;
+};
+
+temp().then((res)=>{
+    console.log(res)
+}).catch(()=>{
+    console.log("ERROR ");
+});
+
+
+// AWAIT : pauses the execution of its surrounding async function until the epromise is setteld. Used only in aysnc function 
+
+function greet2(){
+    return new Promise((resolve , reject)=>{
+        setTimeout(() => {
+            let i = Math.floor(Math.random()*10 +1);
+            if (i>5){
+            console.log("Hello ");
+            resolve();
+            }else{
+                reject("Error");
+            }
+        }, 2000);
+    });
+}
+
+async function call(){
+    try{
+        await greet2();
+        await greet2();
+        await greet2();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+call();
