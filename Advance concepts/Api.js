35f26283dcfa49aed2738c4d476d25cbdec5e472
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-21 11:12:19 +0530

    Practiced API's

---
+++ Advance concepts/Api.js
@@ -0,0 +1,47 @@
+// API : 
+
+// prerequistie
+
+// Api is basically a url.
+// api can be free or paid. In paid we have to add a KEY in the url to access it.
+// For eg :- https://catfact.ninja/fact They give data in JSON format.
+
+// JSON : javacrpit object notation ,it is a format before it was XML. It has key vaue pair like objects. www.json.org
+
+// Json data comes in a form of string . nnot like an object but whole as a string. now to accesss it we use:-
+// "JSON.Parse"
+
+// We have 2 tools for testing api :-
+// a) Hoppscoth
+// b) Poatman
+
+//  AJAX : asynchronous javascript and XML.
+
+// http verbs => get,post,delete;
+
+// Status code => 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
