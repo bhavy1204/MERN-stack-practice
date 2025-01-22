9054fb24d4c0a912008132922758cd44dedf982d
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-22 21:59:54 +0530

    API request practiice

--- Advance concepts/Api.js
+++ Advance concepts/Api.js
@@ -25,11 +25,49 @@
 
 // URL?Key=Value1&Key2=Value2;
 
+// fetch(url);
+
+let url = "https://catfact.ninja/fact";
+
+// fetch(url).
+// then((res) => {
+//     console.log(res);
+
+//     return res.json()
+//     // .then((data)=>{
+//     //     console.log(data);
+//     // })
+// })
+// .then((data)=>{
+//     console.log(data.fact);
+//     return fetch(url);
+// })
+// .then((res)=>{
+//     return res.json();
+// })
+// .then((data2)=>{
+//     console.log(data2.fact);
+// })
+// .catch((err)=>{
+//     console.log(err)
+// });
 
 
+async function getKnowledge(){
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
 
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-
+getKnowledge();
 
 
 
