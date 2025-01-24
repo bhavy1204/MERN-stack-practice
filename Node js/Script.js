6ce37290924317be673d566a7a13805757fce6f5
bhavy1204 <bhavysarwa51@gmail.com>
2025-01-24 20:59:29 +0530

    Node js started

---
+++ Node js/Script.js
@@ -0,0 +1,18 @@
+let a ="Hello ";
+
+for(let i =0;i<3;i++){
+    console.log(i+1);
+}
+console.log(a);
+console.log("Meow");
+
+// console.log(process.version);
+// console.log(process.cwd);
+// console.log(process.release);
+console.log(process.argv);
+
+let args = process.argv;
+
+for(let i=2;i<args.length;i++){
+    console.log("Meow ", args[i]);
+}
