10ab01a9cd6793cc9001b7100f8b8568592f68d1
bhavy1204 <bhavysarwa51@gmail.com>
2025-02-25 08:50:14 +0530

    created mysql with node

--- SQL/index.js
+++ SQL/index.js
@@ -1,16 +1,44 @@
+// Genertaing  fake data and requiring faker
 const { faker } = require('@faker-js/faker');
 
-let randomData = () => {
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.username(), // before version 9.1.0, use userName()
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      password: faker.internet.password(),
-      birthdate: faker.date.birthdate(),
-      registeredAt: faker.date.past(),
-    };
-  }
+// Getting Mysql connection object 
+const mysql = require('mysql2');
 
-  console.log(randomData());
-  
+// Craeting connection 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mernTest',
+  password: '9928875294'
+});
+
+// Creating  queries using connection oject 
+try {
+  connection.query("SHOW TABLES",
+    (err, res) => {
+      if (err) {
+        console.log(err);
+      } 
+      console.log(res);
+    }
+  )
+} catch (error) {
+  console.log(error);
+}
+
+// Will create and connect MYSQL to terminal next time
+
+
+let randomData = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(), // before version 9.1.0, use userName()
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+console.log(randomData());
