512bc04d32799c57230b6b94cd6bfcc21b04289c
bhavy1204 <bhavysarwa51@gmail.com>
2025-02-28 08:49:17 +0530

    Inserted data in bulk using faker

--- SQL/index.js
+++ SQL/index.js
@@ -14,15 +14,33 @@ const connection = mysql.createConnection({
 
 // Writing query in a variable
 // let q = "SHOW TABLES"
+// This way we can only insert data in a limited manner that ie bulk operation are not very practical with this For bul we will use faker 
 let q = "INSERT INTO user(userId,username,email,password) VALUES ?";
 let data = [["1123", "Bhaavy1204", "Bhdavy@Email", "KyuBatau"],
 ["1223", "Ashish1204", "Ahsish@Email", "HeHehehehehe"]
 ];
 
+// Creating bulk data using faker and for loop
+
+let randomData = ()=>{
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password()
+  ]
+}
+
+let bulkData=[];
+for (let i = 0; i <100; i++) {
+  bulkData.push(randomData());
+}
+
+
 // Creating  queries using connection oject 
 try {
   // Just query than we can write simple q but if we are adding values via template(?) than we have to paas both q and data.
-  connection.query(q, [data],
+  connection.query(q, [bulkData],
     (err, res) => {
       if (err) {
         console.log(err);
@@ -44,17 +62,19 @@ connection.end();
 // Will create and connect MYSQL to terminal next time
 
 
-let randomData = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
-}
+// let randomData = () => {
+//   return {
+//     userId: faker.string.uuid(),
+//     username: faker.internet.username(), // before version 9.1.0, use userName()
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//   };
+// }
+
+
 
 
 // To print the generated data using generate data function
