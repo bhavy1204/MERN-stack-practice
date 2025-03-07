4bbbba5bc9fa3da962dcd2d80619849ba281f003
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-07 11:56:48 +0530

    Started Routing with sql

--- SQL/index.js
+++ SQL/index.js
@@ -4,6 +4,9 @@ const { faker } = require('@faker-js/faker');
 // Getting Mysql connection object 
 const mysql = require('mysql2');
 
+const express = require('express');
+const app =express();
+
 // Craeting connection 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -15,49 +18,49 @@ const connection = mysql.createConnection({
 // Writing query in a variable
 // let q = "SHOW TABLES"
 // This way we can only insert data in a limited manner that ie bulk operation are not very practical with this For bul we will use faker 
-let q = "INSERT INTO user(userId,username,email,password) VALUES ?";
-let data = [["1123", "Bhaavy1204", "Bhdavy@Email", "KyuBatau"],
-["1223", "Ashish1204", "Ahsish@Email", "HeHehehehehe"]
-];
+// let q = "INSERT INTO user(userId,username,email,password) VALUES ?";
+// let data = [["1123", "Bhaavy1204", "Bhdavy@Email", "KyuBatau"],
+// ["1223", "Ashish1204", "Ahsish@Email", "HeHehehehehe"]
+// ];
 
 // Creating bulk data using faker and for loop
 
-let randomData = ()=>{
-  return [
-    faker.string.uuid(),
-    faker.internet.username(),
-    faker.internet.email(),
-    faker.internet.password()
-  ]
-}
+// let randomData = ()=>{
+//   return [
+//     faker.string.uuid(),
+//     faker.internet.username(),
+//     faker.internet.email(),
+//     faker.internet.password()
+//   ]
+// }
 
-let bulkData=[];
-for (let i = 0; i <100; i++) {
-  bulkData.push(randomData());
-}
+// let bulkData=[];
+// for (let i = 0; i <100; i++) {
+//   bulkData.push(randomData());
+// }
 
 
 // Creating  queries using connection oject 
-try {
-  // Just query than we can write simple q but if we are adding values via template(?) than we have to paas both q and data.
-  connection.query(q, [bulkData],
-    (err, res) => {
-      if (err) {
-        console.log(err);
-      }
-      // The result is array of objects
-      console.log(res);
-      // console.log(res.length);
-      // console.log(res[0]);
-      // console.log(res[1]);
-    }
-  )
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   // Just query than we can write simple q but if we are adding values via template(?) than we have to paas both q and data.
+//   connection.query(q, [bulkData],
+//     (err, res) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       // The result is array of objects
+//       console.log(res);
+//       // console.log(res.length);
+//       // console.log(res[0]);
+//       // console.log(res[1]);
+//     }
+//   )
+// } catch (error) {
+//   console.log(error);
+// }
 
-// To end the connection otherwise the execution will not stop
-connection.end();
+// // To end the connection otherwise the execution will not stop
+// connection.end();
 
 // Will create and connect MYSQL to terminal next time
 
@@ -79,3 +82,25 @@ connection.end();
 
 // To print the generated data using generate data function
 // console.log(randomData());
+
+
+// Routing with express and sql 
+
+app.get('/',(req,res)=>{
+  let query = "Select count(*) from user;";
+  try {
+    connection.query(query,(err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      res.send(result);
+    })
+  } catch (err) {
+    console.log(err);
+    res.send("Bhai tere DB me dikkat hai chod de coding naai ban ja ! ");
+  }
+  // res.send("Aa gaya maut ka tamasha dekhne ! ");
+});
+
+app.listen(3000,()=>{
+  console.log("Halchal at port 3000 ");
+})
