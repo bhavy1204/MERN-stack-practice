15e3bf80e22c229e6f67f510bc2c8be0b00c743d
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-17 19:23:13 +0530

    Database set up completed

--- Mongo With express/index.js
+++ Mongo With express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const Chat = require("./models/chat");
 
 const path = require("path");
 
@@ -18,11 +19,23 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+// let chat1 = new Chat({
+//     from :"Bhavy",
+//     to :"Ashish",
+//     msg :"BHADVA SALA !! ",
+//     created_at : new Date(),
 
+// });
+
+// chat1.save().then((res)=>{
+//     console.log(res);
+// });
 
 app.get("/",(req,res)=>{
     res.send("Aa gaya firse BC ! ");
 })
+
+
 
 app.listen(3000,()=>{
     console.log("Halchal on port 3000");
---
+++ Mongo With express/init.js
@@ -0,0 +1,60 @@
+// This file is used for inserting sample data in DB for just to check and help in future refrence in devlpment of project This is runned only for once 
+const mongoose = require("mongoose");
+const Chat = require("./models/chat");
+
+
+main().then(()=>{
+    console.log("Le bhai mongo se bhi connect ho gaya ! ");
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let Bulkchats = [
+    {
+    from :"Bhavy",
+    to :"Ashish",
+    msg :"BHADVA SALA !! ",
+    created_at : new Date(),
+    },
+    {
+        from :"Ram",
+        to :"Shaym",
+        msg :"Hehahaha",
+        created_at : new Date(),
+    },
+    {
+        from :"sita",
+        to :"gita",
+        msg :"hari bol !",
+        created_at : new Date(),
+    },
+    {
+        from :"Meow",
+        to :"Tota",
+        msg :"te te te te te te ! ",
+        created_at : new Date(),
+    },
+    {
+        from :"Table",
+        to :"Kursi",
+        msg :"lavdi saali ! ",
+        created_at : new Date(),
+    },
+    {
+        from :"kanishk",
+        to :"Khushal",
+        msg :"Bhai me gay hu ! ",
+        created_at : new Date(),
+    },
+
+];
+
+// chat1.save().then((res)=>{
+//     console.log(res);
+// });
+
+Chat.insertMany(Bulkchats);
