548d3e4961dba332df708acb1b3615db7b71db70
bhavy1204 <bhavysarwa51@gmail.com>
2025-03-20 22:28:55 +0530

    Fix dates and started update route

--- Mongo With express/index.js
+++ Mongo With express/index.js
@@ -68,6 +68,15 @@ app.get("/chats", async (req,res)=>{
     // res.send("Ab tuje chats dekhni hai BKL..");
 });
 
+// EDIT ROUTE
+
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
+// HOME/MAIN ROUTE
 app.get("/",(req,res)=>{
     res.send("Aa gaya firse BC ! ");
 })
