3daac6edc3ec29bdbfe78fca9306b727e7c42102
bhavy1204 <bhavysarwa51@gmail.com>
2025-04-07 08:22:24 +0530

    Completed one to few relationship

--- Mongo Relationships/models/user.js
+++ Mongo Relationships/models/user.js
@@ -8,6 +8,32 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
 }
 
+// MongoDb will also generate Id for address as it sees "address" as a valid schema and thus also provide it id for each individual record
 const userSchema = new Schema({
-    
+    username : String,
+    address:[
+        {
+            _id: false,    // If we dont want id here 
+            location : String,
+            city:String,
+        },
+    ],
 });
+
+const User = mongoose.model("User", userSchema);
+
+// one to few relationship as one user can have multiple addresses.
+const addUsers = async () => {
+    let user1 = new User({
+        username: "Nilla Billa",
+        address:[{
+            location:"Kacchi basti",
+            city:"Dholakpur"
+        }]
+    });
+    user1.address.push({location:"Choti kacchi basti",city:"Meowpura"});
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
