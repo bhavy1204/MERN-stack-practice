a8811816f96d81065204f833f661693834f068f2
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-13 10:16:02 +0530

    props complete

--- React/react-practice/src/CardList.jsx
+++ React/react-practice/src/CardList.jsx
@@ -1,16 +1,17 @@
 import Card from "./card";
 
-let link1="/creation_1.png";
-let link2= "/vite.svg";
+let link1 = "/creation_1.png";
+let link2 = "/vite.svg";
 
-let options = ["heha" , "meow" , "sher" , "kutta"];
-//We will have to wrap the numbers in curly bracket , To use them as a number 
+// let options = [<li>"heha" </li>, <li> "meow"</li>, <li> "sher"</li>, <li>"kutta"</li>];
+let options = [<li>"heha" </li>, <li> "meow"</li>, <li> "sher"</li>, <li>"kutta"</li>];
+const list = options.map((el) => <li>{el}</li>); //we can also wrrite this whole logic at place of rendering insted of creating new variable
 function CardList() {
     return (
         <div className="cardList">
-            <Card link={link1} title="title 1" desc="this is desc 1 " options={options}/> 
-            <Card link={link1} desc="this is desc 2 " options={["raju","kaju","bhaju"]}/>
-            <Card link={link2} title="title 2" desc="this is desc 3 " objOption={{a:"ram",b:"hari",c:"vishnu"}}/>
+            <Card link={link1} title="title 1" desc="this is desc 1 " options={list} />
+            <Card link={link1} desc="this is desc 2 " options={["raju", "kaju", "bhaju"]} />
+            <Card link={link2} title="title 2" desc="this is desc 3 " objOption={{ a: "ram", b: "hari", c: "vishnu" }} />
         </div>
     )
 }
