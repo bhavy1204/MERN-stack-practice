df1298aa1b7610952c595555ff2fde8ff3609ee5
bhavy1204 <bhavysarwa51@gmail.com>
2025-06-20 07:51:36 +0530

    complete lottery gamee

--- React/Object-and-State/src/Lottery.jsx
+++ React/Object-and-State/src/Lottery.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react"
 import "./Lottery.css"
-import { genTicket } from "./helper";
+import { genTicket,sum } from "./helper";
 
-export default function Lottery(){
-    let [ticket,setTicket] = useState(genTicket(3));
+export default function Lottery() {
+    let [ticket, setTicket] = useState(genTicket(3));
+    let isWining = sum(ticket) === 15;
 
+    let createTicket= ()=>{
+        setTicket(genTicket(3));
+    }
     return (
         <>
             <h1> Le bhai laga satta </h1>
+            <h1>{isWining && "Badhai ho tu jit gaya ! "}</h1>
             <div className="number">
                 <span>{ticket[0]}</span>
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
-            <button >Chuu mantar</button>
+            <button onClick={createTicket} >Chuu mantar</button>
         </>
     )
 }
